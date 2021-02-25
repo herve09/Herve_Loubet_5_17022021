50a2d6ae0a4bfaabff78f8e7e4dcdffb3aa93a56
herve <09ariegeois@gmail.com>
2021-02-25 11:03:28 +0100

    sauvegarde js (probleme  affichage image, prix avec decimale et signe €----mise en place du css via scss

--- js/ours.js
+++ js/ours.js
@@ -3,47 +3,52 @@ console.log('bonjour')
 window.onload = function get_body() {
 
     var x = document.getElementsByTagName("body")[0]
+        fetch("http://localhost:3000/api/teddies")
+            .then(res => res.json())
+              
+              .then(res => {
+                    
+                    for (let i = 0; i < res.length; i++) {
+                        let displayCatalogue = document.getElementById('catalogueteddy')
+                    let teddySection = document.createElement('section')
+                    
+                    let img = document.createElement('img')
+                    let name = document.createElement('h4')
+                    let price = document.createElement('p')
+                    let description = document.createElement('description')
+                    let link = document.createElement('a')
+                    
+                    
+                    teddySection.classList.add('teddySection')
+                    img.classList.add('teddy_img')
+                    name.classList.add('teddy_name')
+                    price.classList.add('teddy_price')
+                    description.classList.add('teddy_description')
+                    link.classList.add('teddy_link')
 
-    fetch("http://localhost:3000/api/teddies")
-        .then(res => res.json())
-        /*.then(function (data)*/ 
-        .then(res => {
-          /*  for (var teddy in data) {*/
-          for (let i = 0; i < res.length; i++) {
-              /* var names = name[i] */
-                let displayCatalogue = document.getElementById('catalogueteddy')
-                let teddySection = document.createElement('section')
+                    img.src = res[i].img
+                    
+                    name.textContent = res[i].name
+                    price.textContent = res[i].price 
+                    description.content = res[i].description
+                    link.href="./pages.teddy.html?id=" + res[i].id 
+                    link.textContent = "Plus de détail"
+                    
 
-                displayCatalogue.appendChild(teddySection)
-                /* creation */
-               
-                let name = document.createElement('h4')
+                    teddySection.appendChild(img)
+                    teddySection.appendChild(name)
+                    teddySection.appendChild(price)
+                    teddySection.appendChild(description)
+                    teddySection.appendChild(link)
 
-                /************** 
-                .then(res => res.json())
-        .then(res => {*/
-                teddySection.classList.add('teddyname')
-               
-                name.classList.add('teddy_name')
 
-                /******************* */
-                name.textContent = this.name
 
-                /******************** */
-               
-                teddySection.appendChild(name)
-                displayCatalogue.appendChild(teddySection)
-   /*     )
-            }*/
-                console.log(data[teddy]["name"])
-/********************
-                console.log(data[teddy]["imageUrl"])
-                console.log(data[teddy]["name"])
-                console.log(data[teddy]["price"])
-                */
-            }
-        }
-    )
- }
 
- 
+
+                    displayCatalogue.appendChild(teddySection)
+                    }
+                }
+
+            )
+        }        
+        
